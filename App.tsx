9305f4285f5115ee3b9f1e165a7844ec898d719b
lucaspires-source <pires.lucas94@gmail.com>
2022-10-18 15:03:58 -0300

    instaling apollo client

--- App.tsx
+++ App.tsx
@@ -2,13 +2,26 @@ import { TailwindProvider } from 'tailwind-rn'
 import utilities from './tailwind.json'
 import { NavigationContainer } from '@react-navigation/native'
 import RootNavigator from './navigator/RootNavigator'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'https://tapes.stepzen.net/api/good-llama/__graphql',
+  cache: new InMemoryCache(),
+})
+
 export default function App() {
   return (
-    <NavigationContainer>
-      {/* @ts-ignore - TailwindProvider is missing type definitions */}
-      <TailwindProvider utilities={utilities}>
-        <RootNavigator />
-      </TailwindProvider>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        {/* @ts-ignore - TailwindProvider is missing type definitions */}
+        <TailwindProvider utilities={utilities}>
+          <RootNavigator />
+        </TailwindProvider>
+      </NavigationContainer>
+    </ApolloProvider>
   )
 }
